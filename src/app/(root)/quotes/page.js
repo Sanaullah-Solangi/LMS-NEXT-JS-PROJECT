bd445b90ead7a8d60f5c2d3e2f145b7bad96caed
Sanaullah <sanaullahsolangi817@gmail.com>
2024-10-19 23:13:42 +0500

    key prop is given at its correct place

--- src/app/(root)/quotes/page.js
+++ src/app/(root)/quotes/page.js
@@ -12,11 +12,8 @@ async function Quotes() {
       <div className="container mx-auto  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {quotes.quotes.map((quote) => {
           return (
-            <Link href={`quotes/${quote.id}`}>
-              <div
-                key={quote.id}
-                className="flex flex-col justify-between items-center m-h-[300px] h-[100%] p-4 border border-gray-400 rounded-lg hover:bg-purple-100 cursor-pointer"
-              >
+            <Link key={quote.id} href={`quotes/${quote.id}`}>
+              <div className="flex flex-col justify-between items-center m-h-[300px] h-[100%] p-4 border border-gray-400 rounded-lg hover:bg-purple-100 cursor-pointer">
                 <h1 className="text-center text-2xl">{quote.quote}</h1>
                 <h3 className="text-center text-[18px] mt-3">{quote.author}</h3>
               </div>
